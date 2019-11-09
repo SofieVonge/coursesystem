7be3142f86180a36d076dfd77af503e3e7aff894
dani153i <danielblom90@gmail.com>
2019-11-09 16:38:35 +0100

    Create model SearchCriteria for JPA search

---
+++ src/main/java/dk/kea/kurser/models/SearchCriteria.java
@@ -0,0 +1,60 @@
+package dk.kea.kurser.models;
+
+public class SearchCriteria
+{
+    private String key;
+    private String operation;
+    private Object value;
+    private boolean orPredicate;
+
+    public SearchCriteria() {
+        key = "";
+        operation = "";
+        value = null;
+    }
+
+    public SearchCriteria(String key, String operation, Object value) {
+        this.key = key;
+        this.operation = operation;
+        this.value = value;
+    }
+
+    public SearchCriteria(String key, String operation, Object value, boolean isOrPredicate) {
+        this.key = key;
+        this.operation = operation;
+        this.value = value;
+        this.orPredicate = isOrPredicate;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public boolean isOrPredicate() {
+        return orPredicate;
+    }
+
+    public void setOrPredicate(boolean orPredicate) {
+        this.orPredicate = orPredicate;
+    }
+}
