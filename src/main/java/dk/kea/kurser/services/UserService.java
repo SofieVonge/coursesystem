1f2c3b4f324a3ccb8a73091619040d660debf8a4
dani153i <danielblom90@gmail.com>
2019-11-10 20:21:07 +0100

    Create UserService with saveUser method

---
+++ src/main/java/dk/kea/kurser/services/UserService.java
@@ -0,0 +1,19 @@
+package dk.kea.kurser.services;
+
+import dk.kea.kurser.models.User;
+import dk.kea.kurser.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+}
