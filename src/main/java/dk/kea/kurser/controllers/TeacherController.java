4b14393a0cd4544f63257714a2a352b0f959605d
Dessch0197 <desi0197@stud.kea.dk>
2019-11-11 12:35:26 +0100

    login tjek i teacher

--- src/main/java/dk/kea/kurser/controllers/TeacherController.java
+++ src/main/java/dk/kea/kurser/controllers/TeacherController.java
@@ -2,6 +2,8 @@ package dk.kea.kurser.controllers;
 
 
 import dk.kea.kurser.models.Course;
+import dk.kea.kurser.models.Role;
+import dk.kea.kurser.models.User;
 import dk.kea.kurser.services.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class TeacherController {
@@ -21,27 +25,65 @@ public class TeacherController {
     TeacherService teacherService;
 
 @GetMapping("/create")
-    public String create(){
+    public String create(HttpSession session){
+    User user = (User) session.getAttribute("user");
+
+    if(user != null)
+    {
+        if(!user.getRole().equals(Role.TEACHER))
+        {
+            return "redirect:/";
+        }
+    }
 
     return "createCourse";
 }
 @PostMapping("/create")
-    public String createCourse(@ModelAttribute Course course){
+    public String createCourse(@ModelAttribute Course course, HttpSession session){
+
+    User user = (User) session.getAttribute("user");
+
+    if(user != null)
+    {
+        if(!user.getRole().equals(Role.TEACHER))
+        {
+            return "redirect:/";
+        }
+    }
 
     teacherService.createCourse(course);
     //bed browser om at navigere til index-siden
     return "redirect:/";
 }
     @GetMapping("/delete/{id}")
-    public String deleteCourse(@PathVariable("id") Long id)
+    public String deleteCourse(@PathVariable("id") Long id, HttpSession session)
     {
+        User user = (User) session.getAttribute("user");
+
+        if(user != null)
+        {
+            if(!user.getRole().equals(Role.TEACHER))
+            {
+                return "redirect:/";
+            }
+        }
         teacherService.deleteTeacher(id);
         return "redirect:/";
     }
 
     // få fat id fra stien vha. @PathVariable
     @GetMapping("/update/{id}")
-    public String update(@PathVariable("id") Long id, Model model){
+    public String update(@PathVariable("id") Long id, Model model,HttpSession session){
+
+        User user = (User) session.getAttribute("user");
+
+        if(user != null)
+        {
+            if(!user.getRole().equals(Role.TEACHER))
+            {
+                return "redirect:/";
+            }
+        }
         //tilføj course med id til viewmodel
         model.addAttribute("course", teacherService.findById(id));
         return "update";
@@ -49,7 +91,17 @@ public class TeacherController {
 
     // opdater product - @ModelAttribute bruges til at få fat i product fra post
     @PostMapping("/update")
-    public String updateTeacher(@ModelAttribute Course course){
+    public String updateTeacher(@ModelAttribute Course course,HttpSession session){
+
+        User user = (User) session.getAttribute("user");
+
+        if(user != null)
+        {
+            if(!user.getRole().equals(Role.TEACHER))
+            {
+                return "redirect:/";
+            }
+        }
         //kald update service
         teacherService.updateCourse(course);
         //sikr mod refresh fejl og sletter igen
