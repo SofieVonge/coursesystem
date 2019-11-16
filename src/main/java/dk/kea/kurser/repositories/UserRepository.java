ce4b750a5da45471d231fd475edb003145b7004c
dani153i <danielblom90@gmail.com>
2019-11-16 12:02:28 +0100

    added method findByRole

--- src/main/java/dk/kea/kurser/repositories/UserRepository.java
+++ src/main/java/dk/kea/kurser/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package dk.kea.kurser.repositories;
 
 import dk.kea.kurser.models.Course;
+import dk.kea.kurser.models.Role;
 import dk.kea.kurser.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -24,4 +25,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
     public User findByEmailAndSecret(String email, String secret);
 
     public Set<User> findByEnrolledCourses(Course course);
+
+    public Set<User> findByRole(Role role);
 }
